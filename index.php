c9d0976a43b91ab9023619029508361c91642d4b
Gabriel Paladino <l31rb4g@gmail.com>
2013-09-22 11:27:21 -0300

    ops!

--- index.php
+++ index.php
@@ -64,6 +64,7 @@
     </div>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
         <script>
