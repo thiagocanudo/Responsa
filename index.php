2196de0152b139d646af77fc9312234155f380d6
Thiago Campos <efetive@gmail.com>
2013-09-30 22:43:55 -0300

    Git Ribbon

--- index.php
+++ index.php
@@ -74,7 +74,7 @@
             s.parentNode.insertBefore(g,s)}(document,'script'));
         </script>
         <script src="js/site.js"></script>
-        <a href="https://github.com/thiagocanudo/Responsa" target="blank"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub"></a>
+        <a href="https://github.com/thiagocanudo/Responsa" target="blank"><img style="position: fixed; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub"></a>
         <script defer src="http://sawpf.com/1.0.js"></script>
     </body>
 </html>
